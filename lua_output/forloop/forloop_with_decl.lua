fa7a4bdb44b6c57ed22774dd2f0e6d47d1c6ad49
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2026-01-14 22:29:53 +0500

    Fix leak in loop instrumentation

--- lua_output/forloop/forloop_with_decl.lua
+++ lua_output/forloop/forloop_with_decl.lua
@@ -12,6 +12,20 @@ function cn.for_with_decl.loop_check.a(i, acc)
     --@note This is popped at the end of the loop body
     cn.frames.push_loop()
 
+    --[[
+    This is unideal. For now, our contract has been to push everything CN related
+    into Lua but keep the ownership state, error handling etc in C. Loops are
+    annoying in that they have a whole ownership structure that we have to allocate
+    but then we also use it for CN ownership checking. 
+    
+    Ideally, I'd push this all to Lua but the loop ownership struct is pretty strongly
+    entangled rn with the rest of ownership checking and appears to be fairly
+    optimized there. So that seems unwise.
+    
+    However, calling into C from here is also a half solve. This loop ownership state 
+    isn't being bumped off either since we're not tracking the bump
+    allocator at the callsite so we're in a weird state. Need to commit one way or the other.
+    ]]--
     local ownership_state = cn.c.initialise_loop_ownership_state()
 
     cn.error_stack.push("  for(int i = 0; i < 10; i++)\n  ^~~~~~~~~~~~~~~~~~~~~~~~~~~ ./tests/cn/forloop_with_decl.c:4:3-9:4")
--- lua_output/forloop/forloop_with_decl_with_lua.exec.c
+++ lua_output/forloop/forloop_with_decl_with_lua.exec.c
@@ -10,38 +10,34 @@ int for_with_decl()
 
   signed int ret;
   
-  int acc = 0;
+  signed int acc = 0;
   lua_cn_ghost_add((&acc), sizeof(signed int), lua_cn_get_stack_depth());
 
-  // This is unideal. For now, our contract has been to push everything CN related
-  // into Lua but keep the ownership state, error handling etc in C. Loops are
-  // annoying in that they have a whole ownership structure that we have to allocate
-  // but then we also use it for CN ownership checking. 
-  // It's gross to retain the allocation part here and do everything else in Lua. Need to commit
-  // one way or the other.
-  cn_bump_frame_id loop_id;
-
-  for (
-    signed int i = (lua_cn_ghost_add((&i), sizeof(signed int), lua_cn_get_stack_depth()), 0);
-    //@note from saljuk:
-    // My understanding of the C instrumentation is that we were doing the loop checks here AND
-    // then doing the loop condition. This mean that we'd unconditionally bump the frame for a new
-    // iteration and then be off by 1 at the last check, requiring an extra free after the loop.
-    // To me, rearranging things like this to ensure we only do the bump if we will actually
-    // go inside the loop feels cleaner.
-    (CN_LOAD(i) < 10) &&
-    ({
-      /*@ inv 0i32 <= i; i <= 10i32;
-        acc <= 10i32; @*/
-      loop_id = cn_bump_get_frame_id();
-      lua_cn_for_with_decl_loop_check_a(&i, &acc);
-      true;
-    }); 
-    CN_POSTFIX(i, ++))
   {
-    CN_STORE(acc, CN_LOAD(i));
-    lua_cn_frame_pop_loop();
-    cn_bump_free_after(loop_id);
+    signed int i = 0;
+    lua_cn_ghost_add((&i), sizeof(signed int), lua_cn_get_stack_depth());
+
+    for (;
+      //@note from saljuk:
+      // My understanding of the C instrumentation is that we were doing the loop checks here AND
+      // then doing the loop condition. This meant that we'd unconditionally bump the frame for a new
+      // iteration and then be off by 1 at the last check, requiring an extra free after the loop.
+      // To me, rearranging things like this to ensure we only do the bump if we will actually
+      // go inside the loop feels cleaner.
+      (CN_LOAD(i) < 10) &&
+      ({
+        /*@ inv 0i32 <= i; i <= 10i32;
+          acc <= 10i32; @*/
+        lua_cn_for_with_decl_loop_check_a(&i, &acc);
+        true;
+      }); 
+      CN_POSTFIX(i, ++))
+    {
+      CN_STORE(acc, CN_LOAD(i));
+      lua_cn_frame_pop_loop();
+    }
+
+    lua_cn_ghost_remove((&i), sizeof(signed int));
   }
 
   ret = CN_LOAD(acc);
