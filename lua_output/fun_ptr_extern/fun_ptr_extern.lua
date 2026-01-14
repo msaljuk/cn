9556dac2f323fe92a88d347550e61074728d4c33
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2026-01-14 21:48:01 +0500

    Get fun_ptr_extern working

---
+++ lua_output/fun_ptr_extern/fun_ptr_extern.lua
@@ -0,0 +1,37 @@
+local cn = require("lua_cn_runtime_core")
+
+--[[
+CN spec
+--]]
+
+--[[
+predicate (void) Is_Known_Binop (pointer f) {
+  assert (ptr_eq(f, &f1) || ptr_eq(f, &f2));
+  return;
+}
+@*/
+--]]
+
+local function Is_Known_Binop(ret)
+    return (ret == cn.frames.get_local("f1") or ret == cn.frames.get_local("f2"))
+end
+
+--[[
+Pre/Post conditions
+--]]
+
+cn.get_int_binop = {}
+
+function cn.get_int_binop.postcondition(ret, f1, f2)
+    cn.frames.set_local("f1", f1);
+    cn.frames.set_local("f2", f2);
+
+    cn.error_stack.push("/*@ ensures take X = Is_Known_Binop (return); @*/\n                 ^./tests/cn/fun_ptr_extern.c:34:18:")
+    cn.frames.set_local("X", Is_Known_Binop(ret))
+    cn.assert(cn.frames.get_local("X"), cn.spec_mode.POST)
+    cn.error_stack.pop()
+
+    print("Postcondition Passed in Lua")
+end
+
+return cn
