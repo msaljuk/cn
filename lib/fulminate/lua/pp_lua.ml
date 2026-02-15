40999da6aac3a96ac5fd1600fcd756e9875af368
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2026-02-15 15:34:29 +0000

    Introduce basic Lua AST and get something generating
    commit 0b843efc1f626e9e0a50b498d4f12f33f7d5f81c
    Author: Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
    Date:   Sat Feb 7 13:38:17 2026 +0000
    
       Introduce basic Lua AST and get something generating
    
    commit 963f26d4b64ba8e13c06f9896e55249634c729d4
    Author: Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
    Date:   Fri Feb 6 21:16:52 2026 +0000
    
        Refactor cn-to-ail to bubble up lua stmts entry

---
+++ lib/fulminate/lua/pp_lua.ml
@@ -0,0 +1,34 @@
+module LuaS = Lua_syntax
+include PPrint
+
+let indent block =
+  List.map (fun s -> "    " ^ s) block
+  |> String.concat "\n" 
+
+let rec pp_expr = function
+  | LuaS.Nil -> "nil"
+  | LuaS.Bool b -> string_of_bool b
+  | LuaS.Number_Int i -> string_of_int i
+  | LuaS.Number_Float f -> string_of_float f
+  | LuaS.String s -> "\"" ^ s ^ "\""
+  | LuaS.Symbol id -> id
+  | LuaS.Table (k, v) ->
+      Printf.sprintf "%s.%s" (pp_expr k) (pp_expr v)
+  | LuaS.Call (fn, args) ->
+      let args_str = String.concat ", " (List.map pp_expr args) in
+      Printf.sprintf "%s(%s)" fn args_str
+
+let rec pp_stmt = function
+  | LuaS.Assign (id, e) ->
+      id ^ " = " ^ (pp_expr e)
+  | LuaS.LocalAssign (id, e) -> 
+      "local " ^ id ^ " = " ^ (pp_expr e)
+
+  | LuaS.FunctionDef (fn, args, body) ->
+      let args_list = List.map pp_expr args in
+      let header = Printf.sprintf "function %s(%s)" fn (String.concat ", " args_list) in
+      let indented_body = indent (List.map pp_stmt body) in
+      header ^ "\n" ^ indented_body ^ "\nend"
+  | LuaS.FunctionCall (fn, args) ->
+      pp_expr (LuaS.Call(fn, args))
+  | _ -> ""
