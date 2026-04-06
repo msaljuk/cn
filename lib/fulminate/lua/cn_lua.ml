cbfba888d1c26fe2fe2e0cb35b6bad4851ad4d3f
Saljuk Gondal <62533326+msaljuk@users.noreply.github.com>
2026-04-06 02:52:14 +0100

    Push c function parameters into Lua properly

--- lib/fulminate/lua/cn_lua.ml
+++ lib/fulminate/lua/cn_lua.ml
@@ -23,9 +23,7 @@ let cn_asserts_sym = LuaS.Symbol( "cn.asserts" )
 let cn_error_stack_push_sym = LuaS.Symbol( "cn.error_stack.push" )
 let cn_error_stack_pop_sym  = LuaS.Symbol( "cn.error_stack.pop" )
 let cn_frames_push_fn_sym = LuaS.Symbol( "cn.frames.push_function" )
-let cn_frames_set_local_sym = LuaS.Symbol( "cn.frames.set_local" )
-let c_sym = LuaS.Symbol( "c" )
-let c_sym_addr_suffix = "_addr"
+let cn_locals_sym = LuaS.Symbol("cn.locals")
 let get_type_prefix = "get_"
 let push_type_prefix = "push_"
 
@@ -69,8 +67,8 @@ let convert_c_args_to_wrapper_args (c_args :(CF.Ctype.union_tag * CF.Ctype.ctype
     List.map (
       fun (tag, ctype) ->
         (
-          (Sym.fresh ((Sym.pp_string tag) ^ c_sym_addr_suffix)),
-          (CF.Ctype.no_qualifiers, mk_ctype (CF.Ctype.Pointer (CF.Ctype.no_qualifiers, ctype)), false)
+          tag,
+          (CF.Ctype.no_qualifiers, ctype, false)
         )
     ) c_args
 
@@ -97,11 +95,6 @@ let debug_print_stmts (stmts : lua_statements)
   (List.iter
   (fun (x : lua_statement) -> (print_endline (PP.pp_stmt x)))
   stmts)
-
-let c_sym_to_lua_sym (c_sym : CF.Ctype.union_tag)
-  : LuaS.expr
-  = 
-  LuaS.Symbol(Sym.pp_string c_sym)
 
 let generate_c_fn_wrapper_name (c_fn_name : Sym.t)
  = "lua_cn_" ^ (Sym.pp_string c_fn_name)
@@ -162,6 +155,46 @@ let generate_c_get_lua_state
 
   (binding, decl_stmt)
 
+let generate_c_push_field_into_lua
+  (lua_state_expr : CF.GenTypes.genTypeCategory A.expression)
+  (c_field_expr : CF.GenTypes.genTypeCategory A.expression)
+  (c_type : CF.Ctype.ctype)
+  : CF.GenTypes.genTypeCategory A.expression
+  =
+  let call name args = mk_expr (AilEcall (mk_expr (AilEident (Sym.fresh name)), args)) in
+  let int_const i = mk_expr (A.AilEconst (ConstantInteger (IConstant (Z.of_int i, Decimal, None)))) in
+
+  let final_expr = 
+    begin match rm_ctype c_type with
+      | CF.Ctype.Basic (x) ->
+        begin match x with 
+          | CF.Ctype.Integer (i_type) -> 
+            begin match i_type with
+              | CF.Ctype.Bool -> call "lua_pushboolean" [ lua_state_expr; c_field_expr ]
+              | CF.Ctype.Char -> call "lua_pushlstring" [ lua_state_expr; c_field_expr; int_const(1) ]
+              | CF.Ctype.Signed (_) | CF.Ctype.Unsigned (_) | CF.Ctype.Size_t 
+                -> call "lua_pushinteger" [ lua_state_expr; c_field_expr ]
+              | _ -> exit 2
+            end
+          | CF.Ctype.Floating (f_type) ->
+            begin match f_type with
+              | CF.Ctype.RealFloating (rf_type) ->
+                begin match rf_type with
+                  | CF.Ctype.Float | CF.Ctype.Double | CF.Ctype.LongDouble
+                      -> call "lua_pushfloat" [ lua_state_expr; c_field_expr ]
+                end
+            end
+        end 
+      | CF.Ctype.Pointer (_, _) -> call "lua_pushinteger" [ lua_state_expr; call "lua_convert_ptr_to_int" [ c_field_expr ] ]
+      | CF.Ctype.Struct (s_sym) -> 
+          let addressof_field = A.AilEunary(Address, c_field_expr) in
+          call ("lua_cn_push_" ^ Sym.pp_string s_sym) [ mk_expr addressof_field ]
+      | _ -> exit 2
+    end
+  in
+
+  (final_expr)
+
 let generate_c_fn_wrapper_def 
   (lua_fn_name : string)
   (wrapper_fn_name : string)
@@ -218,16 +251,18 @@ let generate_c_fn_wrapper_def
 
   let arg_names, arg_types = List.split wrapper_fn_args in
 
+  let lua_state_expr = var "L" in
+
   let lua_fn_field_names = List.tl (String.split_on_char '.' lua_fn_name) in
   let generate_getfield field_name =
-    call "lua_getfield" [var "L"; int_const (-1); str_expr field_name];
+    call "lua_getfield" [lua_state_expr; int_const (-1); str_expr field_name];
   in
 
-  let generate_arg_push arg_name =
-    call "lua_pushinteger" [
-        var "L"; 
-        mk_expr (AilEcall (var "lua_convert_ptr_to_int", [var (Sym.pp_string arg_name)]))
-      ];
+  let generate_arg_push (wrapper_fn_arg : (A.ail_identifier * (CF.Ctype.qualifiers * CF.Ctype.ctype * bool))) =
+    let arg_name, arg_type = wrapper_fn_arg in
+    let arg_name_expr = var (Sym.pp_string arg_name) in
+    let _, arg_ctype, _ = arg_type in 
+    A.AilSexpr (generate_c_push_field_into_lua lua_state_expr arg_name_expr arg_ctype)
   in
 
   let lua_state_bs, lua_state_ss = generate_c_get_lua_state in
@@ -237,12 +272,12 @@ let generate_c_fn_wrapper_def
       mk_stmt
       (
         [ lua_state_ss ]
-        @ [ call "lua_rawgeti" [var "L"; var "LUA_REGISTRYINDEX"; mk_expr (AilEcall (var "lua_cn_get_runtime_ref", [])) ]]
+        @ [ call "lua_rawgeti" [lua_state_expr; var "LUA_REGISTRYINDEX"; mk_expr (AilEcall (var "lua_cn_get_runtime_ref", [])) ]]
         @ (List.map generate_getfield lua_fn_field_names)
-        @ (List.map generate_arg_push arg_names)
+        @ (List.map generate_arg_push wrapper_fn_args)
         (*@saljuk TODO: Make this safer by checking the results of p_call and handling them *)
-        @ [ call "lua_pcall" [var "L"; int_const (List.length arg_names); int_const (0); int_const (0) ]]
-        @ [ call "lua_pop" [var "L"; int_const (List.length lua_fn_field_names - 1)] ]
+        @ [ call "lua_pcall" [lua_state_expr; int_const (List.length wrapper_fn_args); int_const (0); int_const (0) ]]
+        @ [ call "lua_pop" [lua_state_expr; int_const (List.length lua_fn_field_names - 1)] ]
       )
   in
   
@@ -327,45 +362,6 @@ let generate_c_fn_struct_size (struct_name : A.ail_identifier)
   in
 
   (decl, def)
-
-let generate_c_push_field_into_lua
-  (lua_state_expr : CF.GenTypes.genTypeCategory A.expression)
-  (c_field_expr : CF.GenTypes.genTypeCategory A.expression)
-  (c_type : CF.Ctype.ctype)
-  : CF.GenTypes.genTypeCategory A.expression
-  =
-  let call name args = mk_expr (AilEcall (mk_expr (AilEident (Sym.fresh name)), args)) in
-  let int_const i = mk_expr (A.AilEconst (ConstantInteger (IConstant (Z.of_int i, Decimal, None)))) in
-
-  let final_expr = 
-    begin match rm_ctype c_type with
-      | CF.Ctype.Basic (x) ->
-        begin match x with 
-          | CF.Ctype.Integer (i_type) -> 
-            begin match i_type with
-              | CF.Ctype.Bool -> call "lua_pushboolean" [ lua_state_expr; c_field_expr ]
-              | CF.Ctype.Char -> call "lua_pushlstring" [ lua_state_expr; c_field_expr; int_const(1) ]
-              | CF.Ctype.Signed (_) | CF.Ctype.Unsigned (_) | CF.Ctype.Size_t 
-                -> call "lua_pushinteger" [ lua_state_expr; c_field_expr ]
-              | _ -> exit 2
-            end
-          | CF.Ctype.Floating (f_type) ->
-            begin match f_type with
-              | CF.Ctype.RealFloating (rf_type) ->
-                begin match rf_type with
-                  | CF.Ctype.Float | CF.Ctype.Double | CF.Ctype.LongDouble
-                      -> call "lua_pushfloat" [ lua_state_expr; c_field_expr ]
-                end
-            end
-        end 
-      | CF.Ctype.Pointer (_, _) -> call "lua_pushinteger" [ lua_state_expr; call "lua_convert_ptr_to_int" [ c_field_expr ] ]
-      | CF.Ctype.Struct (s_sym) -> 
-          call ("lua_cn_push_" ^ Sym.pp_string s_sym) [ c_field_expr ]
-      | _ -> exit 2
-    end
-  in
-
-  (final_expr)
 
 let generate_c_fn_push_struct_name (struct_name : A.ail_identifier)
   : string 
@@ -603,67 +599,18 @@ let generate_lua_push_frame_fn
   (c_fn_args : (CF.Ctype.union_tag * (CF.Ctype.qualifiers * CF.Ctype.ctype * bool)) list)
   : LuaS.stmt
   = 
-
-  (*@saljuk NOTE: Consider hoisting this out if we need to reuse this logic for pre/post/inline
-  stmt parameter reading (most likely)*)
-  let get_arg_expr
-    (arg : (CF.Ctype.union_tag * (CF.Ctype.qualifiers * CF.Ctype.ctype * bool)))
-    = 
-    let get_fn_prefix = LuaS.Field(cn_sym, c_sym) in
-
-    let c_symbol, (_, (c_addr_type : CF.Ctype.ctype), _) = arg in
-    let c_type = get_ctype_without_ptr c_addr_type in
-
-    let type_str = 
-      let get_type type_str = get_type_prefix ^ type_str in
-
-      (match (rm_ctype c_type) with
-        | CF.Ctype.Basic (x) ->
-          (match (x) with 
-            | CF.Ctype.Integer (i_type) -> 
-              (match i_type with
-                | CF.Ctype.Bool -> get_type "bool";
-                | CF.Ctype.Char -> get_type "char";
-                (*@saljuk TODO: Revisit this in the future. *)
-                | CF.Ctype.Signed (_) | CF.Ctype.Unsigned (_) -> get_type "integer";
-                | CF.Ctype.Size_t -> get_type "size_t";
-                | _ -> (""));
-            | CF.Ctype.Floating (f_type) ->
-              match f_type with
-                | CF.Ctype.RealFloating (rf_type) ->
-                  match rf_type with
-                    | CF.Ctype.Float -> get_type "float";
-                    | CF.Ctype.Double | CF.Ctype.LongDouble -> get_type "double");
-        | CF.Ctype.Pointer (_, _) -> get_type "pointer";
-        | CF.Ctype.Struct (s_sym) -> get_type (Sym.pp_string s_sym);
-        | _ -> "")
-    in
-
-    LuaS.Call(
-      Pp_lua.pp_expr (LuaS.Field(get_fn_prefix, LuaS.Symbol(type_str))),
-      [ c_sym_to_lua_sym c_symbol ] )
-  in
-
   let get_args 
     = 
     List.map
     (fun (arg : (CF.Ctype.union_tag * (CF.Ctype.qualifiers * CF.Ctype.ctype * bool))) -> (
-      let get_sym_name_wo_addr_suffix sym =
-        let sym_name_w_suffix = Sym.pp_string sym in
-        let len_a = String.length sym_name_w_suffix in
-        let len_b = String.length c_sym_addr_suffix in
-        String.sub sym_name_w_suffix 0 (len_a - len_b)
-      in
+      let c_sym, _ = arg in
 
-      let c_sym_w_addr, _ = arg in
+      let c_sym_as_lua_expr = LuaS.Symbol(Sym.pp_string c_sym) in
 
-      (LuaS.FunctionCall(
-        Pp_lua.pp_expr cn_frames_set_local_sym,
-        [
-          LuaS.String(get_sym_name_wo_addr_suffix c_sym_w_addr);
-          get_arg_expr arg
-        ]
-      ))
+      LuaS.Assign(
+        Pp_lua.pp_expr (LuaS.Field(cn_locals_sym, c_sym_as_lua_expr)),
+        c_sym_as_lua_expr
+      )
     ))
     c_fn_args
   in
@@ -673,8 +620,8 @@ let generate_lua_push_frame_fn
   let get_arg_name
     (arg : (CF.Ctype.union_tag * (CF.Ctype.qualifiers * CF.Ctype.ctype * bool))) 
     =
-    let c_sym_with_addr, _ = arg in
-    (LuaS.Symbol (Sym.pp_string c_sym_with_addr))
+    let c_sym, _ = arg in
+    (LuaS.Symbol (Sym.pp_string c_sym))
   in
   let lua_fn_args = List.map get_arg_name c_fn_args in
 
